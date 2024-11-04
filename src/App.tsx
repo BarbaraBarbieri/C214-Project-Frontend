408837e53e4a60e597cf8aab4d81191da8be80d7
Alice de Lorenzo Coelho <alicedelorc@gmail.com>
2024-11-04 15:35:08 -0300

    feat: add statistics component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Dash from './components/Dash'
 import Sidebar from './components/Sidebar'
+import Statistics from './components/Statistics'
 
 export default function App() {
   return (
     <div className='flex bg-gray-800'>
       <Sidebar />
-      
+
       <div className='w-full p-12'>
+        {/* Adicionando o componente Statistics */}
+        <Statistics totalIncome="$1,329.00" totalExpense="$1,329.00" />
+        
         <Dash />
       </div>
     </div>
